e1f7c432815ee9aeaffa2fab6f7ed9e26a0cd9a1
Le8wa-dev <savinchev@list.ru>
2019-09-19 12:37:45 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ let gulp = require('gulp'),
     // cssmin = require('gulp-cssmin');
 
 gulp.task('sass', function(){
-    return gulp.src('app/scss/style.scss')
+    return gulp.src('app/scss/**/*.scss')
             .pipe(sass({outputStyle: 'compressed'}))
             .pipe(rename({suffix: '.min'}))
             .pipe(autoprefixer({
@@ -18,16 +18,20 @@ gulp.task('sass', function(){
             .pipe(browserSync.reload({stream: true}))
 });
 
-// gulp.task('style', function(){
-//     return gulp.src([
-//         'node_modules/normalize.css/normalize.css',
-//         'node_modules/slick-carousel/slick/slick.css',
-//         'node_modules/magnific-popup/dist/jquery.magnific-popup.css'
-//     ])
-//         .pipe(concat('libs.min.css'))
-//         .pipe(cssmin())
-//         .pipe(gulp.dest('app/css'))
-// });
+    //  используется пока нет плагина gulp-cssmin
+    // .pipe(concat('libs.css'))
+    // .pipe(cssmin())    если используется плагин cssmin
+    // .pipe(gulp.dest('app/css'))
+
+gulp.task('style', function(){
+    return gulp.src([
+        'node_modules/normalize.css/normalize.css',
+        'node_modules/slick-carousel/slick/slick.css',
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.css'
+    ])
+        .pipe(concat('libs.min.css'))
+        .pipe(gulp.dest('app/css'))
+});
 
 gulp.task('script', function(){
     return gulp.src([
