49beb36ab410d4b8d3e7f46330b9f59b78d3997d
Le8wa-dev <savinchev@list.ru>
2019-09-22 20:12:53 +0300

    added video slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,18 @@
-$(function(){
+$(function () {
 
 
-    $('.slidertop__inner').slick({
-        dots: true,
-        arrows: false
-      });
+  $('.slidertop__inner').slick({
+    dots: true,
+    arrows: false
+  });
 
-    
+  $('.video__slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    arrows: false,
+    variableWidth: true
+  });
+
+
 });
