41bbeb7babcadff430747ee6b19ad5769fd66c35
Le8wa-dev <savinchev@list.ru>
2019-09-24 14:56:54 +0300

    added section sliderbottom

--- app/js/main.js
+++ app/js/main.js
@@ -14,5 +14,21 @@ $(function () {
     variableWidth: true
   });
 
+  $('.sliderbottom__inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    asNavFor: '.sliderbottom__nav'
+  });
+  $('.sliderbottom__nav').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.sliderbottom__inner',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true
+  });
+
 
 });
